cc1b7a9086ed73ad2ebb4006e7a4bfd13e21fdf6
Rubel Biewas <harunbiswasrubel@gmaile.com>
2021-08-21 00:31:39 +0600

    Harun

---
+++ assets/js/theme.js
@@ -0,0 +1,25 @@
+// float navigation 
+
+let addBtn = document.querySelector('.nav-toggle');
+let removeBtn = document.querySelector('.btn-remove')
+let nav = document.querySelector('.main-navigation');
+
+
+addBtn.addEventListener('click', ()=> {
+    nav.style.left = "0"
+})
+removeBtn.addEventListener('click', ()=> {
+    nav.style.left = "-35rem"
+})
+
+
+// active navigation 
+
+let li = document.querySelectorAll('.main-navigation-item');
+        
+        li.forEach(el => {
+            el.addEventListener('click', function(){
+                document.querySelector('.active').classList.remove('active');
+                el.classList.add('active');
+            })
+        })
